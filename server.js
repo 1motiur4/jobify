cc63c7bc8fd0dd92d4710e0dcce784fc0f85052a
Motiur Muhith <1motiur4@gmail.com>
2023-05-26 16:53:42 -0400

    45 done

--- server.js
+++ server.js
@@ -3,15 +3,23 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 
+//db and authenticate user
 import connectDB from "./db/connect.js";
+
+//routers
+import authRouter from "./routes/authRoutes.js";
 
 // middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
+
+app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -20,7 +28,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGODB_URL2);
+    await connectDB(process.env.MONGODB_URL);
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}`);
     });
@@ -29,4 +37,4 @@ const start = async () => {
   }
 };
 
-start()
+start();
