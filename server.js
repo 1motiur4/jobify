f0dcb675c65283fc1b52445571e8c03a3856ff7e
1motiur4 <92241810+1motiur4@users.noreply.github.com>
2023-05-29 22:54:16 -0400

    61 done

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
+import 'express-async-errors'
 
 //db and authenticate user
 import connectDB from "./db/connect.js";
 
 //routers
 import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRoutes.js";
 
 // middleware
 import notFoundMiddleware from "./middleware/not-found.js";
@@ -20,6 +22,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
