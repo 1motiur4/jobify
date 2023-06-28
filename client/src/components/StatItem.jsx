7e3b2080420369e984c6aab1d28e143acd40d6ef
1motiur4 <92241810+1motiur4@users.noreply.github.com>
2023-06-27 23:28:33 -0400

    165 done, basic stats

--- client/src/components/StatItem.jsx
+++ client/src/components/StatItem.jsx
@@ -1,6 +1,17 @@
-const StatItem = () => {
+import Wrapper from "../assets/wrappers/StatItem";
+
+const StatItem = ({ count, title, icon, color, bcg }) => {
   return (
-    <div>StatItem</div>
-  )
-}
-export default StatItem
+    <Wrapper
+      color={color}
+      bcg={bcg}
+    >
+      <header>
+        <span className="count">{count}</span>
+        <div className="icon">{icon}</div>
+      </header>
+      <h5 className="title">{title}</h5>
+    </Wrapper>
+  );
+};
+export default StatItem;
